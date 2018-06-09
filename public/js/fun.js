05dd957a6d2fa5ebb387c840c51a14404c19a453
Jell <jean-louis@jawaninja.com>
2018-06-09 22:48:36 +0200

    Sphere

--- public/js/fun.js
+++ public/js/fun.js
@@ -13,18 +13,30 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+var anglesCount = 100;
+var sphereSize = anglesCount;
+var cameraDist = 100;
+var spikiness = 0;
 
 function makeAngle(offset) {
-  var size = 1000;
   var angle = [];
-  for (var i = 0; i < size; i++) {
-    angle.push(new THREE.Vector3(offset, (i + Math.random() - (size/2))*10, Math.random() * 10))
+  var radian;
+  for (var i = 0; i <= sphereSize; i++) {
+    radianHorizontal = Math.PI * offset / anglesCount;
+    radianVertical = Math.PI * i / sphereSize - Math.PI / 2;
+
+    angle.push(
+      new THREE.Vector3(
+        sphereSize * Math.cos(radianHorizontal) * Math.cos(radianVertical),
+        sphereSize * Math.sin(radianHorizontal) * Math.cos(radianVertical),
+        sphereSize * Math.sin(radianVertical),
+      )
+    )
   }
   return angle;
 }
 
 function makePairSurface(faces, offset, leftLength, rightLength) {
-
   for (var i = 0; i < leftLength - 1; i++) {
     faces.push(
       new THREE.Face3(offset + i+1, offset + i, offset + Math.min(rightLength, i + 1) + leftLength - 1)
@@ -61,26 +73,44 @@ var faces = [];
 var vertices = [];
 
 var angles = [];
-for (var i = 0; i < 2000; i++) {
-  angles.push(makeAngle((i - 1000)*10));
+for (var i = 0; i < anglesCount - 2; i++) {
+  angles.push(makeAngle((i - anglesCount / 2)*10));
 }
+angles.push(angles[0]);
+
 
 triangulate(faces, vertices, angles);
-
 
 var geometry = new THREE.Geometry();
 geometry.vertices = vertices;
 geometry.faces = faces;
 geometry.computeFaceNormals();
 
-var mesh = new THREE.Mesh( geometry, new THREE.MeshNormalMaterial({side: THREE.DoubleSide}) );
+var mesh = new THREE.Mesh( geometry, new THREE.MeshNormalMaterial({
+  side: THREE.DoubleSide,
+  opacity: 0.1,
+  transparent: true
+}));
 scene.add( mesh );
 
-camera.position.z = 600;
+var wireframe = new THREE.WireframeGeometry( geometry );
+
+var line = new THREE.LineSegments( wireframe );
+line.material.depthTest = true;
+line.material.opacity = 0.5;
+line.material.transparent = true;
+
+scene.add( line );
+
+camera.position.z = cameraDist;
 
 function animate() {
   requestAnimationFrame( animate );
-  mesh.rotation.y += 0.01;
+  controls.update();
   renderer.render( scene, camera );
 }
+
+var controls = new THREE.OrbitControls( camera );
+controls.update();
+
 animate();
